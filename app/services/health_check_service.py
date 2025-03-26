06d22d9c94fbd76396352a4dc8534915b5e38dab
Tharun-Maheswararao <maheswararao.t@northeastern.edu>
2025-03-25 22:42:56 -0400

    updated service

--- app/services/health_check_service.py
+++ app/services/health_check_service.py
@@ -1,17 +1,12 @@
 from app.models.model import HealthCheck
 from app.models.database import db
 from app import logger
-from aws_embedded_metrics import metric_scope
+from aws_embedded_metrics import metric_scope, MetricsLogger
 import time
 
-@metric_scope
-def emit_health_metrics(metrics, success: bool, duration: float):
-    metrics.set_namespace("WebAppMetrics")
-    metrics.put_metric("HealthCheck_DB_Duration", duration, "Milliseconds")
-    metrics.put_metric("HealthCheck_Status", 1 if success else 0, "None")
-
 # DB insert method
-def insert_health_check():
+@metric_scope
+def insert_health_check(metrics: MetricsLogger) -> bool:
     logger.info("Inserting health check record into DB")
     start_time = time.time()
 
@@ -21,13 +16,17 @@ def insert_health_check():
         db.session.commit()
 
         duration = (time.time() - start_time) * 1000
-        emit_health_metrics(success=True, duration=duration)
+        metrics.set_namespace("WebAppMetrics")
+        metrics.put_metric("HealthCheck_DB_Duration", duration, "Milliseconds")
+        metrics.put_metric("HealthCheck_Status", 1)
         logger.info("Health check inserted successfully")
         return True
     except Exception as e:
         db.session.rollback()
         duration = (time.time() - start_time) * 1000
-        emit_health_metrics(success=False, duration=duration)
+        metrics.set_namespace("WebAppMetrics")
+        metrics.put_metric("HealthCheck_DB_Duration", duration, "Milliseconds")
+        metrics.put_metric("HealthCheck_Status", 0)
         logger.error(f"Error inserting health check: {str(e)}", exc_info=True)
         print(f"Error inserting health check: {e}")
         return False
