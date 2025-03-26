df5db9d1f2a9ac124e12b2e18749f794ba1e941f
Tharun-Maheswararao <maheswararao.t@northeastern.edu>
2025-03-26 14:21:59 -0400

    debugging healthz api

--- app/services/health_check_service.py
+++ app/services/health_check_service.py
@@ -6,28 +6,28 @@ import time
 
 # DB insert method
 def insert_health_check():
-    logger.info("Inserting health check record into DB")
-    start_time = time.time()
+    # logger.info("Inserting health check record into DB")
+    # start_time = time.time()
 
     try:
         new_check = HealthCheck()
         db.session.add(new_check)
         db.session.commit()
 
-        duration = (time.time() - start_time) * 1000
-        emit_health_metrics(success=True, duration=duration)
-        logger.info("Health check inserted successfully")
+        # duration = (time.time() - start_time) * 1000
+        # emit_health_metrics(success=True, duration=duration)
+        # logger.info("Health check inserted successfully")
         return True
     except Exception as e:
         db.session.rollback()
-        duration = (time.time() - start_time) * 1000
-        emit_health_metrics(success=False, duration=duration)
-        logger.error(f"Error inserting health check: {str(e)}", exc_info=True)
+        # duration = (time.time() - start_time) * 1000
+        # emit_health_metrics(success=False, duration=duration)
+        # logger.error(f"Error inserting health check: {str(e)}", exc_info=True)
         print(f"Error inserting health check: {e}")
         return False
 
-@metric_scope
-def emit_health_metrics(metrics, success: bool, duration: float):
-    metrics.set_namespace("WebAppMetrics")
-    metrics.put_metric("HealthCheck_DB_Duration", duration, "Milliseconds")
-    metrics.put_metric("HealthCheck_Status", 1 if success else 0, "None")
+# @metric_scope
+# def emit_health_metrics(metrics, success: bool, duration: float):
+#     metrics.set_namespace("WebAppMetrics")
+#     metrics.put_metric("HealthCheck_DB_Duration", duration, "Milliseconds")
+#     metrics.put_metric("HealthCheck_Status", 1 if success else 0, "None")
